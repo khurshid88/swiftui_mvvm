7c30ba9490f1fed9cad90e77a58401f4f9557125
nzinsoft <49134360+nzinsoft@users.noreply.github.com>
2021-04-26 09:24:04 +0900

    Initial Commit

---
+++ swiftui_mvvmTests/swiftui_mvvmTests.swift
@@ -0,0 +1,33 @@
+//
+//  swiftui_mvvmTests.swift
+//  swiftui_mvvmTests
+//
+//  Created by User on 2021/04/26.
+//
+
+import XCTest
+@testable import swiftui_mvvm
+
+class swiftui_mvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
